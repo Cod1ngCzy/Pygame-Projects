82781820ad8db08b42a64445f9f407438dd77e28
Naobi Quines <codingguru.czy@gmail.com>
2025-02-28 16:52:18 +0800

    Added Game Logic

--- Pacman/main.py
+++ Pacman/main.py
@@ -19,14 +19,22 @@ class Game():
 
         # Sprites
         self.all_sprites = pygame.sprite.Group()
-        self.player = Player(self.all_sprites)
+        self.pacman = Pacman(self.all_sprites)
         pygame.key.set_repeat(500,100)
 
 
     def handle_events(self):
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+
+                mdx = round(mouse_x // TILE_SIZE)
+                mdy = round(mouse_y // TILE_SIZE)
+
+                GRID_MAP[mdy][mdx] = '1'
+
     
      
     def draw_grid(self):
@@ -36,6 +44,12 @@ class Game():
          # y Loop to Draw Vertical
          for y in range(0, HEIGHT, TILE_SIZE):
               pygame.draw.line(self.display_screen, LIGHTGREY,(0,y), (WIDTH,y))
+
+         # Draw Walls
+         for row_index, row in enumerate(GRID_MAP):
+            for col_index, cell in enumerate(row):
+                if cell == '1':
+                    pygame.draw.rect(self.display_screen,'blue',(col_index * TILE_SIZE, row_index * TILE_SIZE, TILE_SIZE, TILE_SIZE))
             
 
     def run(self):
@@ -50,7 +64,7 @@ class Game():
             self.display_screen.fill('black')
 
             # Draw and Handle Sprites
-            self.all_sprites.update()
+            self.all_sprites.update(dt)
             self.all_sprites.draw(self.display_screen)
 
             # Draw Grid
--- Pacman/sprites.py
+++ Pacman/sprites.py
@@ -1,34 +1,52 @@
 from settings import *
 
-class Player(pygame.sprite.Sprite):
+class Pacman(pygame.sprite.Sprite):
     def __init__(self, groups):
         super().__init__(groups)
         self.image =  pygame.Surface((TILE_SIZE,TILE_SIZE))
         self.image.fill('yellow')
         self.rect = self.image.get_frect()
         self.pos = pygame.Vector2(0,0)
+        self.direction = pygame.Vector2(0,0)
+        self.speed = 5
+        self.move_timer = 0
     
-    def move(self):
-        # Get Input for Directional Movement
-        keys = pygame.key.get_just_pressed()
-        self.pos.x = (keys[pygame.K_d] - keys[pygame.K_a])
-        self.pos.y = (keys[pygame.K_s] - keys[pygame.K_w])
-        print(self.rect.center)
-    
+    def input_movement(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_d]:
+            self.direction = pygame.Vector2(1,0)
+        elif keys[pygame.K_a]:
+            self.direction = pygame.Vector2(-1,0)
+        elif keys[pygame.K_w]:
+            self.direction = pygame.Vector2(0,-1)
+        elif keys[pygame.K_s]:
+            self.direction = pygame.Vector2(0,1)
+            
     def grid_boundary_collision(self):
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
+            self.pos.x = WIDTH / TILE_SIZE
         if self.rect.left < 0:
             self.rect.left = 0
+            self.pos.x = 0
         if self.rect.top < 0:
             self.rect.top = 0
+            self.pos.y = 0 
         if self.rect.bottom > HEIGHT:
             self.rect.bottom = HEIGHT
+            self.pos.y = HEIGHT / TILE_SIZE
 
-    def update(self, *args, **kwargs):
-        # Movement Application
-        self.move()
-        self.grid_boundary_collision()
-        self.rect.x += self.pos.x * TILE_SIZE
-        self.rect.y += self.pos.y * TILE_SIZE
-     
+    def update(self, dt):
+        self.input_movement()
+        self.move_timer += dt
+
+        if self.move_timer >= 1 / self.speed:
+            self.move_timer = 0
+
+            self.pos += self.direction
+            self.rect.x = self.pos.x * TILE_SIZE 
+            self.rect.y = self.pos.y * TILE_SIZE
+
+            self.grid_boundary_collision()
+
+        
